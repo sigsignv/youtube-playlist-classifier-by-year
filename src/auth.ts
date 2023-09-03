c57ebae0f5c83c0453eb35e1c050f71062d9071e
Sigsign <sig@signote.cc>
2023-09-03 17:38:08 +0900

    Add OAuth2 authentication

---
+++ src/auth.ts
@@ -0,0 +1,33 @@
+import { OAuth2Client } from 'google-auth-library'
+import { google } from 'googleapis'
+import { createInterface } from 'node:readline/promises'
+import { getConfig, setConfig } from './token.ts'
+
+export async function auth(): Promise<OAuth2Client> {
+    const token = await getConfig()
+    const client = new google.auth.OAuth2(token.id, token.secret, 'urn:ietf:wg:oauth:2.0:oob')
+    if (token.credentials) {
+        client.credentials = token.credentials
+        return client
+    }
+
+    const url = client.generateAuthUrl({
+        access_type: 'offline',
+        scope: ['https://www.googleapis.com/auth/youtube'],
+    })
+    console.log('Auth URL: ', url)
+
+    const terminal = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    })
+    const answer = await terminal.question('Enter code: ')
+    terminal.close()
+    const response = await client.getToken(answer)
+
+    token.credentials = response.tokens
+    await setConfig(token)
+
+    client.credentials = response.tokens
+    return client
+}
---
+++ src/token.ts
@@ -0,0 +1,47 @@
+import { readFile, writeFile } from 'node:fs/promises'
+import { dirname, resolve } from 'node:path'
+import { Credentials } from 'google-auth-library'
+
+export type Token = {
+    id: string
+    secret: string
+    credentials: Credentials | null
+}
+
+function getConfigPath(): string {
+    return resolve(dirname(__dirname), './config.json')
+}
+
+async function readConfig(filepath: string): Promise<Token> {
+    const contents = await readFile(filepath, { encoding: 'utf-8' })
+    const json = JSON.parse(contents)
+    const token: Token = {
+        id: json['CLIENT_ID'] ?? '',
+        secret: json['CLIENT_SECRET'] ?? '',
+        credentials: json['CREDENTIALS'] ?? null
+    }
+    return token
+}
+
+async function writeConfig(filepath: string, token: Token): Promise<void> {
+    const json = {
+        CLIENT_ID: token.id,
+        CLIENT_SECRET: token.secret,
+        CREDENTIALS: token.credentials,
+    }
+    const contents = JSON.stringify(json, null, 2)
+    return await writeFile(filepath, contents, {
+        encoding: 'utf-8',
+        flag: 'w',
+    })
+}
+
+export function getConfig(): Promise<Token> {
+    const path = getConfigPath()
+    return readConfig(path)
+}
+
+export function setConfig(token: Token): Promise<void> {
+    const path = getConfigPath()
+    return writeConfig(path, token)
+}
