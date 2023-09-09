6427e7187df01e73ba7397a3bac33fa200cb8c25
Sigsign <sig@signote.cc>
2023-09-09 15:06:30 +0900

    Add groupVideosByYear()

--- src/classifier.ts
+++ src/classifier.ts
@@ -1,6 +1,7 @@
-import { isAfter, startOfMonth, startOfYear } from 'date-fns'
+import { format, isAfter, startOfMonth, startOfYear, subMonths } from 'date-fns'
 import { OAuth2Client } from "google-auth-library"
-import { getOwnPlaylists } from "./playlist"
+import { createPlaylist, getOwnPlaylists } from "./playlist"
+import { addVideoIntoPlaylist, containsVideoInPlaylists, getVideosFromPlaylist } from './video'
 
 type YaerlyWatchLists = {
     [key: string]: string
@@ -46,7 +47,7 @@ function getPeriod(groups?: { [key: string]: string } | null): string | null {
     return null
 }
 
-export async function getYearlyWatchLists(client: OAuth2Client) {
+export async function getYearlyWatchLists(client: OAuth2Client): Promise<YaerlyWatchLists> {
     const re = /^WL ((?<year>\d{4})|(?<month>\d{4}-\d{2}))$/
     const yearlyWatchLists: YaerlyWatchLists = {}
 
@@ -59,4 +60,35 @@ export async function getYearlyWatchLists(client: OAuth2Client) {
     }
 
     return yearlyWatchLists
+}
+
+async function addVideoWithPeriod(client: OAuth2Client, lists: YaerlyWatchLists, period: string, videoId: string): Promise<boolean> {
+    let playlistId = lists[period]
+    if (!playlistId) {
+        const playlist = await createPlaylist(client, `WL ${period}`)
+        playlistId = playlist.id
+        lists[period] = playlist.id
+        console.log('Create Playlist: ', playlist.title)
+    }
+    return await addVideoIntoPlaylist(client, playlistId, videoId)
+}
+
+export async function groupVideosByYear(client: OAuth2Client, playlistId: string): Promise<void> {
+    const yearlyWatchLists = await getYearlyWatchLists(client)
+    const containsVideos = await containsVideoInPlaylists(client, Object.values(yearlyWatchLists))
+
+    const lists = await getVideosFromPlaylist(client, playlistId)
+    const videos = lists.filter((video) => !containsVideos(video.id))
+
+    const sixMonthAgo = startOfMonth(subMonths(new Date(), 6))
+    for (const video of videos) {
+        const date = new Date(video.publishedAt)
+        if (isAfter(date, sixMonthAgo)) {
+            const period = format(date, 'yyyy-MM')
+            await addVideoWithPeriod(client, yearlyWatchLists, period, video.id)
+        } else {
+            const period = format(date, 'yyyy')
+            await addVideoWithPeriod(client, yearlyWatchLists, period, video.id)
+        }
+    }
 }
