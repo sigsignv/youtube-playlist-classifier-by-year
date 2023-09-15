11cbf5a6b5516c40670c736df5e07dbe95ae4e8c
Sigsign <sig@signote.cc>
2023-09-16 01:33:28 +0900

    Remove unnecessary type check

--- src/auth/token.ts
+++ src/auth/token.ts
@@ -29,12 +29,5 @@ export function parseToken(json: string): Token {
 }
 
 export function dumpToken(token: Token): string {
-    if (typeof token.CLIENT_ID !== 'string') {
-        throw new Error('[dumpToken] Require Token.CLIENT_ID as string')
-    }
-    if (typeof token.CLIENT_SECRET !== 'string') {
-        throw new Error('[dumpToken] Require Token.CLIENT_SECRET as string')
-    }
-
     return JSON.stringify(token, null, 2)
 }
