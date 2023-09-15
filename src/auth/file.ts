3075f806ec334cd37c78a259d9eadd2ad8a5ed38
Sigsign <sig@signote.cc>
2023-09-16 00:44:06 +0900

    Remove unnecessary async/await

--- src/auth/file.ts
+++ src/auth/file.ts
@@ -1,14 +1,14 @@
 import { readFile } from 'node:fs/promises'
 import { default as writeFileAtomic } from 'write-file-atomic'
 
-export async function readConfigFile(filepath: string): Promise<string> {
-    return await readFile(filepath, {
+export function readConfigFile(filepath: string): Promise<string> {
+    return readFile(filepath, {
         encoding: 'utf-8',
     })
 }
 
-export async function writeConfigFile(filepath: string, data: string): Promise<void> {
-    return await writeFileAtomic(filepath, data, {
+export function writeConfigFile(filepath: string, data: string): Promise<void> {
+    return writeFileAtomic(filepath, data, {
         encoding: 'utf-8',
         fsync: true,
     })
