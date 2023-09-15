3a8d360e96f2b0bda1437cef3760c6e233397366
Sigsign <sig@signote.cc>
2023-09-16 01:22:35 +0900

    Using type guard with unknown type

--- src/auth/token.ts
+++ src/auth/token.ts
@@ -4,9 +4,26 @@ export type Token = {
     REFRESH_TOKEN?: string
 }
 
-export function parseToken(data: string): Token {
-    const config: Token = JSON.parse(data)
+function isToken(token: unknown): token is Token {
+    if (typeof token !== 'object' || token === null) {
+        return false
+    }
+    if (!('CLIENT_ID' in token)) {
+        return false
+    }
+    if (!('CLIENT_SECRET' in token)) {
+        return false
+    }
 
+    return true
+}
+
+export function parseToken(json: string): Token {
+    const config: unknown = JSON.parse(json)
+
+    if (!isToken(config)) {
+        throw new Error('[parseToken] Require CLIENT_ID / CLIENT_SECRET')
+    }
     if (typeof config.CLIENT_ID !== 'string') {
         throw new Error('[parseToken] Require CLIENT_ID')
     }
