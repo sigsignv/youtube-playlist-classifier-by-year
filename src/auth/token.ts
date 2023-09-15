082811aa5c863c07043366f820ef9c9887184728
Sigsign <sig@signote.cc>
2023-09-16 01:32:47 +0900

    Move type check into type guard

--- src/auth/token.ts
+++ src/auth/token.ts
@@ -8,10 +8,10 @@ function isToken(token: unknown): token is Token {
     if (typeof token !== 'object' || token === null) {
         return false
     }
-    if (!('CLIENT_ID' in token)) {
+    if (!('CLIENT_ID' in token) || typeof token.CLIENT_ID !== 'string') {
         return false
     }
-    if (!('CLIENT_SECRET' in token)) {
+    if (!('CLIENT_SECRET' in token) || typeof token.CLIENT_SECRET !== 'string') {
         return false
     }
 
@@ -22,13 +22,7 @@ export function parseToken(json: string): Token {
     const config: unknown = JSON.parse(json)
 
     if (!isToken(config)) {
-        throw new Error('[parseToken] Require CLIENT_ID / CLIENT_SECRET')
-    }
-    if (typeof config.CLIENT_ID !== 'string') {
-        throw new Error('[parseToken] Require CLIENT_ID')
-    }
-    if (typeof config.CLIENT_SECRET !== 'string') {
-        throw new Error('[parseToken] Require CLIENT_SECRET')
+        throw new Error('[parseToken] Require CLIENT_ID / CLIENT_SECRET as string')
     }
 
     return config
