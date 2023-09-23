8a1db3fa9054be690f20b9a207a8201d6cc91e95
Sigsign <sig@signote.cc>
2023-09-23 22:51:59 +0900

    Rewrite with zod

--- src/auth/token.ts
+++ src/auth/token.ts
@@ -1,30 +1,15 @@
-export type Token = {
-    CLIENT_ID: string
-    CLIENT_SECRET: string
-    REFRESH_TOKEN?: string
-}
+import { z } from 'zod'
 
-function isToken(token: unknown): token is Token {
-    if (typeof token !== 'object' || token === null) {
-        return false
-    }
-    if (!('CLIENT_ID' in token) || typeof token.CLIENT_ID !== 'string') {
-        return false
-    }
-    if (!('CLIENT_SECRET' in token) || typeof token.CLIENT_SECRET !== 'string') {
-        return false
-    }
+const Token = z.object({
+    CLIENT_ID: z.string(),
+    CLIENT_SECRET: z.string(),
+    REFRESH_TOKEN: z.string().optional()
+})
 
-    return true
-}
+export type Token = z.infer<typeof Token>
 
 export function parseToken(json: string): Token {
-    const token: unknown = JSON.parse(json)
-    if (!isToken(token)) {
-        throw new Error('[parseToken] Require CLIENT_ID / CLIENT_SECRET as string')
-    }
-
-    return token
+    return Token.parse(JSON.parse(json))
 }
 
 export function dumpToken(token: Token): string {
