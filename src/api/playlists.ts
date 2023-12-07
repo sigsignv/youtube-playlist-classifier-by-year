8c9dc7b8b4b0cd92a457f50c6219c1948ec5c237
Sigsign <sig@signote.cc>
2023-12-07 15:25:45 +0900

    Add getOwnPlaylists()

--- src/api/playlists.ts
+++ src/api/playlists.ts
@@ -1,10 +1,5 @@
-import { OAuth2Client } from 'google-auth-library'
 import { z } from 'zod'
 import { YouTubeClient } from './factory'
-
-export interface PlaylistOptions {
-    auth: OAuth2Client
-}
 
 const playlist = z.object({
     kind: z.string(),
@@ -14,6 +9,11 @@ const playlist = z.object({
 })
 
 export type Playlist = z.infer<typeof playlist>
+
+export type Playlists = {
+    nextPageToken?: string
+    items: Playlist[]
+}
 
 export async function createPlaylist(youtube: YouTubeClient, title: string): Promise<Playlist> {
     const resp = await youtube.playlists.insert({
@@ -36,4 +36,34 @@ export async function createPlaylist(youtube: YouTubeClient, title: string): Pro
 
 export async function dropPlaylist(youtube: YouTubeClient, id: string): Promise<void> {
     await youtube.playlists.delete({ id })
+}
+
+export async function getOwnPlaylists(youtube: YouTubeClient, pageToken?: string): Promise<Playlists> {
+    const resp = await youtube.playlists.list({
+        part: ['snippet'],
+        maxResults: 50,
+        mine: true,
+        pageToken: pageToken ?? '',
+    })
+
+    if (!Array.isArray(resp.data.items)) {
+        return { items: [] }
+    }
+
+    const list: Playlist[] = []
+    for (const item of resp.data.items) {
+        list.push(
+            playlist.parse({
+                kind: item.kind,
+                id: item.id,
+                title: item.snippet?.title,
+                publishedAt: item.snippet?.publishedAt,
+            }),
+        )
+    }
+
+    return {
+        nextPageToken: resp.data.nextPageToken ?? undefined,
+        items: list,
+    }
 }
