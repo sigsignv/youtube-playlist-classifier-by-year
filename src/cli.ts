281bf8e6af5bebbfac62aed66a4be16e0c70c8ed
Sigsign <sig@signote.cc>
2023-12-13 00:29:02 +0900

    Using commander

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,18 @@
-import { dirname, resolve } from 'node:path'
-import { getYouTubeClient } from "./auth/client"
+import { Command } from 'commander'
 
 async function main() {
-    const client = await getYouTubeClient(resolve(dirname(__dirname), './config.json'))
-    console.log(client)
+    const program = new Command()
+
+    program
+        .name('ypp')
+        .version('0.1.0')
+        .description('A YouTube playlist manager')
+
+    program.parse(process.argv)
+
+    if (program.args.length === 0) {
+        program.help()
+    }
 }
 
 main()
